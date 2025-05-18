5ca94bdb315006a67fb20ada0e52624fb69bad26
MohibShaikh <mohibuddin9@gmail.com>
2025-05-18 17:17:45 +0500

    BUg Fix

--- backend/ecom_backend/urls.py
+++ backend/ecom_backend/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
     path('api/', include('payments.urls')),
