409a4b9035637c699ae690afbdc99db24f2015b0
MohibShaikh <mohibuddin9@gmail.com>
2025-05-18 23:41:39 +0500

    finale 2

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Cart, CartItem, Order, OrderItem
+from .models import Order, OrderItem
 
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
