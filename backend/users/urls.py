2967ba37ac378c05c54b144ca94824d7df0a281a
MohibShaikh <mohibuddin9@gmail.com>
2025-05-18 17:50:55 +0500

    template fixing

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('profile/', views.profile_page, name='profile_page'),
     
     # API views
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
-    path('profile/picture/', views.ProfilePictureUpdateView.as_view(), name='profile-picture'),
+    path('api/register/', views.RegisterView.as_view(), name='register'),
+    path('api/login/', views.LoginView.as_view(), name='login'),
+    path('api/profile/', views.UserProfileView.as_view(), name='profile'),
+    path('api/profile/picture/', views.ProfilePictureUpdateView.as_view(), name='profile-picture'),
 ] 
